1c253b2f11db8011ebcf7f9f4e270a01ea9d580f
Vinay jain <jainv823@gmail.com>
2020-11-03 16:51:55 +0530

    Built Header-component(Home-page)

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+  apiKey: "AIzaSyCUNcA_oAcVlJvybXdM9bGo2RjSgI_PIa8",
+  authDomain: "clone-5fa05.firebaseapp.com",
+  databaseURL: "https://clone-5fa05.firebaseio.com",
+  projectId: "clone-5fa05",
+  storageBucket: "clone-5fa05.appspot.com",
+  messagingSenderId: "765535331764",
+  appId: "1:765535331764:web:12f1271ee530024c61bee1",
+  measurementId: "G-M6P0NHKK8F",
+};
